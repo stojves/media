9fdc445af74ec36684d926acfc23e6d4ed931b69
Vesna <stojves@gmail.com>
2020-03-02 20:45:45 +0100

    promenili smo paket u Television.java

---
+++ src/main/java/media/Television.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package media;
+
+/**
+ *
+ * @author Vesna
+ */
+public class Television {
+    int volume = 0;
+    int currentProgram = 1;
+    boolean turnOn;
+
+    public void setVolume(int vv){
+        volume = vv;
+    }
+
+    public int getVolume(){
+        return volume;
+    }
+
+    public void setCurrentProgram(int cp){
+        currentProgram = cp;
+    }
+    public int getCurrentProgram(){
+        return currentProgram;
+    }
+
+}
