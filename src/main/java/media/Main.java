5f4cd51f855c188fbf3733a2f3b62922a2c5c4cd
Vesna <stojves@gmail.com>
2020-03-02 21:14:47 +0100

    druga verzija

--- src/main/java/media/Main.java
+++ src/main/java/media/Main.java
@@ -16,7 +16,24 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Television t1 = new Television();
+        t1.setVolume(23);
+        t1.setCurrentProgram(45);
+        System.out.println("Jacina zvuka je "+t1.getVolume()+" a kanal je "+t1.getCurrentProgram());
+    
+        Radio b92 = new Radio();
+        b92.setFmFrequency(92.5);
+        b92.setAmFrequency(0);
+        b92.setBand('F');
+
+        Radio s = new Radio();
+        s.setFmFrequency(100.8);
+        s.setAmFrequency(0);
+        s.setBand('F');
+
+        System.out.println("Radio B92 ima frekvenciju "+b92.getFmFrequency());
+        
     }
 
 }
