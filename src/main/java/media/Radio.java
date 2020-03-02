bff771d8d587970e422a80737411ae58a16d44e4
Vesna <stojves@gmail.com>
2020-03-02 21:00:21 +0100

    prva verzija

---
+++ src/main/java/media/Radio.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package media;
+
+/**
+ *
+ * @author Vesna
+ */
+public class Radio {
+    double fmFrequency = 91.8;
+    double amFrequency = 600;
+    char band = 'A';
+
+    public void setFmFrequency(double fm){
+        fmFrequency = fm;
+    }
+
+    public int getFmFrequency(){
+        return fmFrequency;
+    }
+
+    public void setAmFrequency(double am){
+        amFrequency = am;
+    }
+
+    public double getAmFrequency(){
+        return amFrequency;
+    }
+
+    public void setBand(char bb){
+        band = bb;
+    }
+
+    public double getBand(){
+        return band;
+    }
+
+    
+}
